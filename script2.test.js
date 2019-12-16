5db5892f2927d669deb20218dd77913f82f79dc0
char502 <char502@hotmail.com>
2019-12-16 18:01:35 +0000

    all tests passing

--- script2.test.js
+++ script2.test.js
@@ -24,7 +24,29 @@ it("calls swapi to get people with a promise", () => {
   });
 });
 
-// expect.assertions(1); promises retuen a pending state so getPeople is going to get returned before actually receive the data
+it("getPeople returns count and results", () => {
+  //Mocking the fetch
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5]
+        })
+    })
+  );
+
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.co/api/people");
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
+
+// =======================================
+// expect.assertions(1); promises return a pending state so getPeople is going to get returned before actually receive the data
 // ======
 // How to tell the test to - don't just pass this test because nothing happened and just returned a promise with a pending state,
 // wait until this is actually done and then keep going
@@ -45,3 +67,18 @@ it("calls swapi to get people with a promise", () => {
 // ======
 // A simpler way:
 // Just return the promise
+// ======
+// Mocks
+
+// Can fake a function
+// Just pretend to have it running
+
+// so can SPY on the bahaviour of a function - that is called indirectly by some other code
+//rather than just testing the output and waiting for the async code to complete
+
+// Mock functions are also known as "spies", because
+// they let you spy on the behavior of a function that is
+// called indirectly by some other code, rather than just
+// testing the output. You can create a mock function with
+// jest.fn(). If no implementation is given, the mock
+// function will return undefined when invoked.
